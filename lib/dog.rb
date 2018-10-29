ff07d9c5e4e662d10f6e07846b3cb8ed2281eec9
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-29 23:31:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,13 @@
 class Dog 
-  def name=(dog_name)
+  def initialize(dog_name)
     @dogs_name = dog_name
   end 
   
-  def name(dog_name)
+  def name
     @dog_name
   end
-end 
+  
+  def name=(new_name)
+     @dog_name = new_name
 
+end
