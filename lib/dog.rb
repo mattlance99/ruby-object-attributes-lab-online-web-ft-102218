8930b2e80960cddb93950eb258826a1edd967963
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-29 23:52:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,5 +6,12 @@ class Dog
   
   def name=(dogs_new_name)
     @name = dogs_new_name
+    
+  def breed 
+     @breed
+  end 
+   
+  def breed=(new_breed_name) 
+    @breed = new_breed_name
   end
 end
