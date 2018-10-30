669358059086f38962ba88dd3facd2b6346b6381
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-30 00:03:10 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  def name
+    @name
+  end
+  
+  def name=(dogs_new_name)
+    @name = dogs_new_name
+  end
+  
+  def breed 
+     @breed
+  end 
+   
+  def breed=(new_breed_name) 
+    @breed = new_breed_name
+  end
+end
